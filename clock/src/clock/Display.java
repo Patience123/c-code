da86002000296b710b1a6f3dd8ac0823bc94df08
Patience123 <773581321@qq.com>
2017-04-15 21:58:13 +0800

    first

---
+++ clock/src/clock/Display.java
@@ -0,0 +1,35 @@
+package clock;
+
+public class Display {
+	private int value = 0;
+	private int limit = 0;
+	
+	public Display(int limit)
+	{
+		this.limit = limit;
+	}
+	
+	public void increase()
+	{
+		value++;
+		if(value == limit)
+		{
+			value = 0;
+		}
+	}
+	
+	public int getvalue()
+	{
+		return value;
+	}
+	
+	public static void main(String[] args) {
+		Display d = new Display(24);
+		for( ;; )
+		{
+			d.increase();
+			System.out.println(d.getvalue());
+		}
+	}
+
+}
